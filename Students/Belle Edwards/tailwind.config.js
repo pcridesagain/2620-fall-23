0acdddedbfc9f5bd77f2fa5420438ce3b1f70587
DocSmudge <91097394+DocSmudge@users.noreply.github.com>
2023-09-23 17:37:55 -0600

    Working on adding hover effect to each date

---
+++ Students/Belle Edwards/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        snoopy: "#ffffff",
+      },
+    },
+  },
+};
+
+
+//hover:bg-indigo-400 rounded-full w-10 h-10
+// right now im 
