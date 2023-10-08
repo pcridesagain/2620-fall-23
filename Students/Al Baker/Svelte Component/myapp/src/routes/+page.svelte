b015be6652e4c5fb4225343f8cbfa06214c4f463
Al Baker <baker.allison.m@gmail.com>
2023-10-08 11:58:59 -0600

    Fixes UVU-98
    Create separate component page for Buttons

--- Students/Al Baker/Svelte Component/myapp/src/routes/+page.svelte
+++ Students/Al Baker/Svelte Component/myapp/src/routes/+page.svelte
@@ -1,51 +1,36 @@
 <script>
+	import Buttons from '$lib/Buttons.svelte';
+
+	let size = true;
+
+	function toggleSize() {
+		size = !size;
+	}
 </script>
 
 <h1 class="text-2xl m-2">Svelte Component - Button Icons</h1>
-<h2 class="text-xl m-2 mb-4">Al Baker, DWDD 2620</h2>
+<h2 class="text-xl m-2">Al Baker, DWDD 2620</h2>
 
-<button
-	class="rounded-full bg-primaryBlue block m-2 h-12 w-12 active:bg-darkBlue disabled:bg-darkGray group"
->
-	<i class="fa-regular fa-circle text-lg text-white group-disabled:text-lightGray" />
-</button>
-<button
-	class="rounded-full bg-lighterBlue block m-2 h-12 w-12 active:bg-lightBlue disabled:bg-darkGray group"
->
-	<i class="fa-regular fa-circle text-lg text-darkBlue group-disabled:text-lightGray" />
-</button>
-<button
-	class="rounded-full border-2 border-lightBlue block m-2 h-12 w-12 active:border-primaryBlue disabled:border-darkGray group"
->
-	<i
-		class="fa-regular fa-circle text-lg text-lightBlue group-active:text-primaryBlue group-disabled:text-darkGray"
-	/>
-</button>
-<button class="rounded-full block m-2 h-12 w-12 active:bg-lightBlue group">
-	<i
-		class="fa-regular fa-circle text-lg text-lightBlue group-active:text-primaryBlue group-disabled:text-darkGray"
-	/>
-</button>
+<p class="m-2">Show small buttons or large buttons?</p>
+<div class="m-2 mb-4">
+	<button on:click={toggleSize} class="rounded-full bg-darkGray py-2 px-4 text-white mr-2"
+		>Large</button
+	>
+	<button on:click={toggleSize} class="rounded-full bg-darkGray py-2 px-4 text-white">Small</button>
+</div>
 
-<button
-	class="rounded-full bg-primaryBlue block m-2 h-8 w-8 active:bg-darkBlue disabled:bg-darkGray group"
->
-	<i class="fa-regular fa-circle fa-xs text-white group-disabled:text-lightGray" />
-</button>
-<button
-	class="rounded-full bg-lighterBlue block m-2 h-8 w-8 active:bg-lightBlue disabled:bg-darkGray group"
->
-	<i class="fa-regular fa-circle fa-xs text-darkBlue group-disabled:text-lightGray" />
-</button>
-<button
-	class="rounded-full border-2 border-lightBlue block m-2 h-8 w-8 active:border-primaryBlue disabled:border-darkGray group"
->
-	<i
-		class="fa-regular fa-circle fa-xs text-lightBlue group-active:text-primaryBlue group-disabled:text-darkGray"
-	/>
-</button>
-<button class="rounded-full block m-2 h-8 w-8 active:bg-lightBlue group">
-	<i
-		class="fa-regular fa-circle fa-xs text-lightBlue group-active:text-primaryBlue group-disabled:text-darkGray"
-	/>
-</button>
+<div class="bg-black p-2">
+	{#if size}
+		<Buttons type="primary" size="lg" />
+		<Buttons type="secondary" size="lg" />
+		<Buttons type="outline" size="lg" />
+		<Buttons type="transparent" size="lg" />
+	{/if}
+
+	{#if !size}
+		<Buttons type="primary" size="sm" />
+		<Buttons type="secondary" size="sm" />
+		<Buttons type="outline" size="sm" />
+		<Buttons type="transparent" size="sm" />
+	{/if}
+</div>
