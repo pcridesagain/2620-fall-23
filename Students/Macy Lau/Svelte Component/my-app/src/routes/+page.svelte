63524010a77f4843f3ec154b40b60da2b31f0cea
Macy Lau <macylau94@gmail.com>
2023-10-12 20:40:57 -0600

    did some clean up and finished component

---
+++ Students/Macy Lau/Svelte Component/my-app/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    
+</script>
+
+<div class="bg-black w-96 h-1/2 p-4">
+    <div class="flex flex-col">
+        <div class="mb-4">
+            <label class="text-white" for="input-field">Input Label</label>
+            <input class="bg-black box-border h-4 w-80 p-4 border-2 border-gray-700 rounded-md focus:outline-none text-white" type="text" placeholder="Input text">
+        </div>
+        <div class="mb-4">
+            <label class="text-white" for="input-field">Input Label</label>
+            <input class="bg-black box-border h-4 w-80 p-4 border-2 border-indigo-600 rounded-md placeholder-white focus:outline-none text-white" type="text" placeholder="Input text">
+        </div>
+        <div class="mb-4">
+            <label class="text-white" for="input-field">Input Label</label>
+            <input class="bg-black box-border h-4 w-80 p-4 border-2 border-gray-700 rounded-md placeholder-white focus:outline-none text-white" type="text" placeholder="Input text">
+        </div>
+        <div class="mb-4">
+            <label class="text-white" for="input-field">Input Label</label>
+            <input class="bg-black box-border h-4 w-80 p-4 border-2 border-red-700 rounded-md placeholder-white focus:outline-none text-white" type="text" placeholder="Input text">
+        </div>
+        <div class="mb-4">
+            <label class="text-white" for="input-field">Input Label</label>
+            <input class="bg-zinc-900 box-border h-4 w-80 p-4 rounded-md placeholder:text-gray-700 focus:outline-none text-white" type="text" placeholder="Input text">
+        </div>
+    </div>
+
+</div>
