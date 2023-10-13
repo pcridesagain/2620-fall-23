7ad52bacb2976fc8a7e8a684a5147014629aefb6
Joonsung-Byun <122327073+Joonsung-Byun@users.noreply.github.com>
2023-10-12 20:17:18 -0600

    Adding Svelte component files

---
+++ Students/Joonsung Byun/Svelte Component/src/nocounter2.svelte
@@ -0,0 +1,11 @@
+<div class="w-48 bg-black h-10 mb-8">
+    <div class="flex justify-center align-center p-1">
+      <div class="w-32 h-8 border border-gray-400 border-r-0 rounded-l-3xl">
+        <p class="text-white text-center text-lg">-</p>
+      </div>
+  
+      <div class="w-32 h-8 border border-gray-400 rounded-r-3xl">
+        <p class="text-center text-indigo-700 text-lg ">+</p>
+      </div>
+    </div>  
+  </div>
