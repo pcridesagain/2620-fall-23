6bcd5f7d5634dc6fdc8f52f956cb939d1b326621
Will <113927655+willmiles00@users.noreply.github.com>
2023-10-12 20:33:11 -0600

    Adding Svelte component files

---
+++ Students/Will Miles/Svelte Component/myapp/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Popup from '.Popup.svelte';
+</script>
+
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+<main class='flex flex-wrap bg-black justify-center h-screen items-center'>
+
+
+
+<Popup> </Popup>
+	
+	</main>
+
+
---
+++ Students/Will Miles/Svelte Component/myapp/src/routes/popup.svelte
@@ -0,0 +1,18 @@
+<script>
+    let item = 'item'
+        import Button from 'button.svelte';
+        </script>
+    
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    
+    <div class='flex flex-wrap flex-col  items-center bg-gray-800 w-3/4 py-8 rounded-xl' id='popup'>
+        
+    <h1 class='text-center text-3xl my-8 font-extrabold'>
+    Remove	{item}?
+    </h1>
+    <p class='text-gray-400 text-center mb-8'>Are you sure you want to remove this {item} from your cart?</p>
+            
+            <Button hover='bg-indigo-800' bgColor='bg-indigo-600' value='Sure'/>
+            <Button hover='text-white' textColor='text-gray-400' value='No, thanks'/>
+        
+    </div>
