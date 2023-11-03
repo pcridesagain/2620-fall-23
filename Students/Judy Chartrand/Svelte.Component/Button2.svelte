00170c0723720aa2dd333024001a0ac90dd3e82d
Judy Chartrand <143059647+Jchart3@users.noreply.github.com>
2023-11-02 18:06:48 -0600

    Delete Students/Judy Chartrand/Svelte.Component/Button2.svelte

--- Students/Judy Chartrand/Svelte.Component/Button2.svelte
+++
@@ -1,5 +0,0 @@
-<div class="flex justify-evenly pt-3">
-    <button class="border border-gray-500 rounded-full px-3 py-3  hover:bg-gray-500"></button>
-    <button class="border border-indigo-400  rounded-full px-3 py-3  hover:bg-indigo-400"></button>
-    <button class="border border-gray-500 rounded-full px-3 py-3  hover:bg-gray-500"></button>
-    </div>
