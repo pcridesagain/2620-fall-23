3dcee9059e15180560608f640ce0b45eddc74f87
Judy Chartrand <143059647+Jchart3@users.noreply.github.com>
2023-11-02 18:06:03 -0600

    Update App.svelte

--- Students/Judy Chartrand/Svelte.Component/App.svelte
+++ Students/Judy Chartrand/Svelte.Component/App.svelte
@@ -1,41 +1,32 @@
 <script>
-	import Tab from "./Tab.svelte"
-	import Button from "./Button.svelte"
-	import Paragraph from "./Paragraph.svelte"
-	import Button2 from "./Button2.svelte"
-	import Paragraph2 from "./Paragraph2.svelte"
-	import Button3 from "./Button3.svelte"
-	import Paragraph3 from "./Paragraph3.svelte"
-	import Button4 from "./Button4.svelte"
-	import Paragraph4 from "./Paragraph4.svelte"
-
-
-
-
-	
-	
-	
+  let sections = [
+    { buttons: [1, 2], titleColors: ['indigo-400', 'gray-500'], buttonColors: ['indigo-400', 'gray-500'] },
+    { buttons: [2, 1, 2], titleColors: ['gray-500', 'gray-500','gray-500'], buttonColors: ['gray-500','indigo-400','gray-500' ] },
+    { buttons: [2, 2, 1, 2], titleColors: ['gray-500', 'gray-500', 'indigo-400', 'gray-500'], buttonColors: ['gray-500', 'gray-500', 'indigo-400', 'gray-500'] },
+    { buttons: [2, 2, 2, 2, 1], titleColors: ['gray-500', 'gray-500', 'gray-500', 'indigo-400', 'gray-500'], buttonColors: ['gray-500', 'gray-500', 'gray-500', 'indigo-400', 'gray-500'] },
+    // Add more sections as needed
+  ];
 </script>
 
 <svelte:head>
-<link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"/>
+  <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" />
 </svelte:head>
 
-<Button />
-<Paragraph />
-<Tab />
+{#each sections as section, i}
+  <nav class="flex-no-wrap relative flex w-full items-center justify-between bg-neutral-100 py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4" data-te-navbar-ref></nav>
 
-<Button2 />
-<Paragraph2 />
-<Tab />
+  <div class="flex justify-evenly pt-{i + 6}">
+    {#each section.buttons as button, j}
+      <button class="border {section.buttonColors[j] === 'indigo-400' ? 'border-indigo-400' : 'border-gray-500'} rounded-full px-3 py-3 hover:bg-{section.buttonColors[j]}"></button>
+    {/each}
+  </div>
 
-<Button3 />
-<Paragraph3 />
-<Tab />
-
-<Button4 />
-<Paragraph4 />
-<Tab />
+  <div class="flex justify-evenly pt-2">
+    {#each section.titleColors as color, k}
+      <p class="text-{color}">Title</p>
+    {/each}
+  </div>
+{/each}
 
 
 
