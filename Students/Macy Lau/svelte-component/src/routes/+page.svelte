522a9ba001375d24751a4cece7854364421fcdec
Macy Lau <macylau94@gmail.com>
2023-11-09 15:14:02 -0700

    fixed text color

--- Students/Macy Lau/svelte-component/src/routes/+page.svelte
+++ Students/Macy Lau/svelte-component/src/routes/+page.svelte
@@ -6,32 +6,33 @@
     let redBorder = 'border-red-700';
     let defaultBg = 'bg-black';
     let inputStyle = 'box-border h-4 w-80 p-4 rounded-md focus:outline-none';
+    let inputText = 'text-white';
 
 </script>
 <div class="bg-black w-96 h-1/2 p-4">
     <div>
         <label class="text-white" for="input-field">{label}</label>
-        <input class="{inputStyle} + {defaultBg} + border-2 + {defaultBorder}" type="text" placeholder={placeholder}>
+        <input class="{inputStyle} + {defaultBg} + border-2 + {defaultBorder} + {inputText}" type="text" placeholder={placeholder}>
     </div>
 
     <div>
         <label class="text-white" for="input-field">{label}</label>
-        <input class="{inputStyle} + {defaultBg} + border-2 + {blueBorder}" type="text" placeholder={placeholder}>
+        <input class="{inputStyle} + {defaultBg} + border-2 + {blueBorder} + {inputText}" type="text" placeholder={placeholder}>
     </div>
 
     <div>
         <label class="text-white" for="input-field">{label}</label>
-        <input class="{inputStyle} + {defaultBg} + border-2 + {defaultBorder}" type="text" placeholder={placeholder}>
+        <input class="{inputStyle} + {defaultBg} + border-2 + {defaultBorder} + {inputText}" type="text" placeholder={placeholder}>
     </div>
 
     <div>
         <label class="text-white" for="input-field">{label}</label>
-        <input class="{inputStyle} + {defaultBg} + border-2 + {redBorder}" type="text" placeholder={placeholder}>
+        <input class="{inputStyle} + {defaultBg} + border-2 + {redBorder} + {inputText}" type="text" placeholder={placeholder}>
     </div>
 
     <div>
         <label class="text-white" for="input-field">{label}</label>
-        <input class="{inputStyle} + bg-zinc-900" type="text" placeholder={placeholder}>
+        <input class="{inputStyle} + bg-zinc-900 + {inputText}" type="text" placeholder={placeholder}>
     </div>
 
     
