9d60a40650380295ce9b0cd47fab6642113db529
Joonsung-Byun <122327073+Joonsung-Byun@users.noreply.github.com>
2023-11-15 20:26:36 -0700

    Adding Svelte component files

---
+++ Students/Joonsung Byun/Storybook Component/myapp/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+<link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+</svelte:head>
+
+<script>
+    import Card from "../stories/Card.svelte";
+    import InputComponent from "../stories/InputComponent.svelte";
+</script>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<InputComponent size="small" textColor="#000" backgroundColor="#e0e0e0" placeholder="Hello Patrick this is Small" primary/>
+<p>Hello World</p>
+<Card title="Card Component" content="This is a Small card component." backgroundColor="#ffffff" borderColor="#000000" size="small"/>
+
---
+++ Students/Joonsung Byun/Storybook Component/myapp/src/stories/Card.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let title = 'Card Title';
+    export let content = 'Card Content';
+    export let backgroundColor = '#ffffff';
+    export let borderColor = '#000000';
+    export let size = 'medium';
+  </script>
+  
+  <style>
+    .card {
+      border: 1px solid var(--border-color, #ddd);
+      border-radius: 8px;
+      background-color: var(--background-color, #fff);
+      padding: 16px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+  
+    h2 {
+      margin-bottom: 8px;
+    }
+  
+    .small {
+      width: 200px;
+    }
+  
+    .medium {
+      width: 300px;
+    }
+  
+    .large {
+      width: 400px;
+    }
+  </style>
+  
+  <div class="card {size}" style="background-color: {backgroundColor}; border-color: {borderColor}">
+    <h2>{title}</h2>
+    <p>{content}</p>
+  </div>
