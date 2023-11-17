c8b1ee9ff21cc48fae7b3b190bba00cea651349e
savsalmon <savannah.b.hilton@gmail.com>
2023-11-16 19:47:58 -0700

    got the savannahButton going

---
+++ Students/Savannah Hilton/SvelteComponent/myapp/src/stories/savannahButton.stories.js
@@ -0,0 +1,55 @@
+import savannahButton from './savannahButton.svelte';
+
+// More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
+const meta = {
+	title: 'Example/savannahButton',
+	component: savannahButton,
+	tags: ['autodocs'],
+	argTypes: {
+		side: 'right',
+		size: 'short',
+		style: 'filledIn'
+		/*
+    backgroundColor: { control: 'color' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+      */
+	}
+}; //satisfies Meta<Button>;
+
+// export default meta;
+// type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/svelte/writing-stories/args
+export const LongFilledIn: Story = {
+	args: {
+		side: 'right',
+		size: 'long',
+		style: 'filledIn'
+	}
+};
+
+export const LongOutline: Story = {
+	args: {
+		side: 'right',
+		size: 'long',
+		style: 'outline'
+	}
+};
+
+export const ShortFilledIn: Story = {
+	args: {
+		side: 'right',
+		size: 'short',
+		style: 'filledIn'
+	}
+};
+
+export const ShortOutline: Story = {
+	args: {
+		side: 'right',
+		size: 'short',
+		style: 'outline'
+	}
+};
