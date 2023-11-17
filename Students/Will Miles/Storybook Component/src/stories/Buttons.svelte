20dd8f02f4960e2d71cd136f5cc0832a187323ca
Will <113927655+willmiles00@users.noreply.github.com>
2023-11-16 18:32:53 -0700

    Adding Svelte component files
    Adding Svelte component files

---
+++ Students/Will Miles/Storybook Component/src/stories/Buttons.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let value;
+	export let bgColor;
+	export let textColor
+	export let hover
+</script>
+
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+<button class='{bgColor} hover:{hover} {textColor} p-3 rounded-xl w-3/4'>
+	{value}
+</button>
