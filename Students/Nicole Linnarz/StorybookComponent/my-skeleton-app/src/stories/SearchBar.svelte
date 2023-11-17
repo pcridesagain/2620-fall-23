be05c5043d3c7ec6afa7e762fbbbd19c23672757
nico-mikaela17 <mikaelafs17@gmail.com>
2023-11-16 19:27:42 -0700

    icons work now

--- Students/Nicole Linnarz/StorybookComponent/my-skeleton-app/src/stories/SearchBar.svelte
+++ Students/Nicole Linnarz/StorybookComponent/my-skeleton-app/src/stories/SearchBar.svelte
@@ -1,10 +1,12 @@
 <script>
+    import "@fortawesome/fontawesome-free/css/all.min.css"
+
+	import '../app.postcss';
 	let display = false;
 	function handleFocus() {
 		display = !display;
 	}
 
-	
 	let inputValue = '';
 
 	function resetForm() {
@@ -20,7 +22,7 @@
 				type="text"
 				bind:value={inputValue}
 				placeholder="Search"
-				class="outline-none p-2 ml-0 mr-0 m-2 w-[12rem] input-field bg-[#202325] text-lg text-left focus:w-[10rem] text-white"
+				class="outline-none border-none p-2 ml-0 mr-0 m-2 w-[12rem] input-field bg-[#202325] text-lg text-left focus:w-[10rem] text-white"
 				on:focus={handleFocus}
 				on:focusout={handleFocus}
 			/>
@@ -28,9 +30,11 @@
 				<i class=" bg-[#202325] rounded-r-lg text-white p-3.5 fa fa-light fa-microphone" />
 			{/if}
 			{#if display}
-				<button 
-				on:click= {resetForm}
-				value="reset" class=" bg-[#202325] rounded-r-lg text-white p-2.5">X</button>
+				<button
+					on:click={resetForm}
+					value="reset"
+					class=" bg-[#202325] rounded-r-lg text-white p-2.5">X</button
+				>
 				<button class="text-gray-400 text-lg bg-black ml-2">Cancel</button>
 			{/if}
 		</div>
--- Students/Nicole Linnarz/StorybookComponent/my-skeleton-app/tailwind.config.js
+++ Students/Nicole Linnarz/StorybookComponent/my-skeleton-app/tailwind.config.js
@@ -8,7 +8,11 @@ export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
-		extend: {},
+		extend: {
+			listStyleImage:{
+				searchIcon: 'url("img/searchIcon.png")'
+			}
+		},
 	},
 	plugins: [
 		forms,
