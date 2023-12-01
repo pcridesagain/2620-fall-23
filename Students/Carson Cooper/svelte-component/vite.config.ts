3e0e7e5e7c9ca266f791542f9e07695fe83330c4
Savitaraxe <carson.wayne.cooper@gmail.com>
2023-11-30 19:49:04 -0700

    storybook changes

--- Students/Carson Cooper/svelte-component/vite.config.ts
+++ Students/Carson Cooper/svelte-component/vite.config.ts
@@ -1,6 +1,10 @@
+// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  build: {
+    outDir: 'dist', // Adjust this according to your project structure
+  },
 });
